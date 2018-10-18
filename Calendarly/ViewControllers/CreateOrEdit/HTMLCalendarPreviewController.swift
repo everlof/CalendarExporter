50840617d63306262ddbadb831648c4cd6c75796
David Everlöf <everlof@gmail.com>
2018-10-18 09:44:39 +0200

    Remove old HTML traces

--- Calendarly/ViewControllers/CreateOrEdit/HTMLCalendarPreviewController.swift
+++ Calendarly/ViewControllers/CreateOrEdit/HTMLCalendarPreviewController.swift
@@ -1,31 +1,6 @@
 import UIKit
 
-class HTMLCalendarPreviewController: UIViewController, HTMLCalendarDelegate {
-
-//    let webView = UIWebView()
-//
-//    let calendar: HTMLCalendar
-//
-//    var renderer: HTMLCalendarRenderer!
-
-//    var webViewInset: UIEdgeInsets = .zero {
-//        didSet {
-//            leftConstraint.constant = webViewInset.left
-//            rightConstraint.constant = -webViewInset.right
-//            topConstraint.constant = webViewInset.top
-//        }
-//    }
-//
-//    var webViewBorder: Bool = false {
-//        didSet {
-//            if webViewBorder {
-//                webView.layer.borderWidth = 1 / UIScreen.main.scale
-//                webView.layer.borderColor = UIColor.boneConstrastDarker.cgColor
-//            } else {
-//                webView.layer.borderWidth = 0.0
-//            }
-//        }
-//    }
+class HTMLCalendarPreviewController: UIViewController {
 
     var leftConstraint: NSLayoutConstraint!
     var topConstraint: NSLayoutConstraint!
@@ -37,8 +12,6 @@ class HTMLCalendarPreviewController: UIViewController, HTMLCalendarDelegate {
     init(design: Design) {
         calendarView = CalendarView(design: design)
         super.init(nibName: nil, bundle: nil)
-//        self.calendar.delegate = self
-//        webView.isUserInteractionEnabled = false
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -49,9 +22,6 @@ class HTMLCalendarPreviewController: UIViewController, HTMLCalendarDelegate {
         super.viewDidLoad()
 
         view.backgroundColor = .boneWhiteColor // UIColor(red: 0xDD/0xFF, green: 0xDD/0xFF, blue: 0xDD/0xFF, alpha: 0xFF)
-
-//        webView.isOpaque = false
-//        webView.backgroundColor = .white
 
         view.addSubview(calendarView)
         calendarView.translatesAutoresizingMaskIntoConstraints = false
@@ -68,17 +38,5 @@ class HTMLCalendarPreviewController: UIViewController, HTMLCalendarDelegate {
         heightContraint = calendarView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         heightContraint.isActive = true
     }
-
-    func reload() {
-//        heightContraint.constant = ((view.frame.width - webViewInset.left - webViewInset.right) * 1.41428571)
-//        webView.loadHTMLString(calendar.export(), baseURL: nil)
-    }
-
-    // MARK: - HTMLCalendarDelegate
-
-    func contentDidChange() {
-        reload()
-    }
-
 
 }
